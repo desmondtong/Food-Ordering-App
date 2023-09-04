36eb02fd426a6dbccef2a6d92eb42f84a6de2f57
Desmond Tong <tysdesmond2@gmail.com>
2023-09-04 10:19:13 +0800

    import based on ts syntax

---
+++ Backend/server.ts
@@ -0,0 +1,41 @@
+require("dotenv").config();
+
+import express, { Express } from "express";
+import helmet from "helmet";
+import { rateLimit } from "express-rate-limit";
+import cors from "cors"; //enables anyone to use the api
+
+// const connectDB = require("./src/db/db"); //import database
+// connectDB();
+
+// const transactions = require("./src/routers/transactions");
+// const listings = require("./src/routers/listings");
+// const auth = require("./src/routers/auth");
+// const images = require("./src/routers/images");
+
+//allows api to be called 100 times within 15min interval
+const limit = rateLimit({
+  windowMs: 1 * 60 * 1000, //1min
+  max: 1000,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+const app: Express = express();
+app.use(cors());
+app.use(helmet());
+app.use(limit);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", (req, res) => {
+  res.send("hello");
+});
+// app.use("/api", transactions);
+// app.use("/auth", auth);
+// app.use("/api", images);
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`server has started on port ${PORT}`);
+});
