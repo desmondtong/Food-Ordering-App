a27fa25770c125c373b8ec90156031cac6615c62
Desmond Tong <tysdesmond2@gmail.com>
2023-09-04 13:24:16 +0800

    create routers & controller directories; npm i pg

--- Backend/server.ts
+++ Backend/server.ts
@@ -1,16 +1,16 @@
 require("dotenv").config();
 
 import express, { Express } from "express";
-import helmet from "helmet";
-import { rateLimit } from "express-rate-limit";
 import cors from "cors"; //enables anyone to use the api
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 // const connectDB = require("./src/db/db"); //import database
 // connectDB();
 
 // const transactions = require("./src/routers/transactions");
 // const listings = require("./src/routers/listings");
-// const auth = require("./src/routers/auth");
+import auth from "./src/routers/auth";
 // const images = require("./src/routers/images");
 
 //allows api to be called 100 times within 15min interval
@@ -28,11 +28,9 @@ app.use(limit);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/", (req, res) => {
-  res.send("hello");
-});
+// app.use("/auth",)
 // app.use("/api", transactions);
-// app.use("/auth", auth);
+app.use("/auth", auth);
 // app.use("/api", images);
 
 const PORT = process.env.PORT || 5001;
---
+++ Backend/src/routers/auth.ts
@@ -0,0 +1,35 @@
+import express from "express";
+const router = express.Router();
+
+// const {
+//   seedAuth,
+//   register,
+//   getAccountById,
+//   getAllAccount,
+//   login,
+//   refresh,
+//   updateProfile,
+// } = require("../controllers/auth");
+import { register, login } from "../controllers/auth";
+
+// const { auth } = require("../middleware/auth");
+// const {
+//   validateRegistrationData,
+//   validateLoginData,
+//   validateRefreshToken,
+//   validateUpdateProfile,
+// } = require("../validators/auth");
+
+// const checkValid = require("../middleware/checkValid");
+
+// router.get("/seed", seedAuth);
+// router.get("/accounts", getAllAccount);
+// router.get("/accounts/:id", getAccountById);
+// router.put("/register", validateRegistrationData, checkValid, register);
+router.put("/register", register);
+// router.post("/login", validateLoginData, checkValid, login);
+router.post("/login", login);
+// router.post("/refresh", validateRefreshToken, checkValid, refresh);
+// router.patch("/update/:id", validateUpdateProfile, updateProfile);
+
+export default router;
