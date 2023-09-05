d114c6ede348d3cbbaad268478ba6a3a3af59635
Desmond Tong <tysdesmond2@gmail.com>
2023-09-05 23:12:07 +0800

    Add endpoint to add item into cart

--- Backend/src/controllers/carts.ts
+++ Backend/src/controllers/carts.ts
@@ -15,8 +15,53 @@ const createCart = async (req: Request, res: Response) => {
   }
 };
 
-const addItemToCart = async (req: Request, res: Response) => {
-  const add = await pool.query("INSERT INTO carts");
+const getCartById = async (req: Request, res: Response) => {
+  try {
+    const cart = await pool.query(
+      "SELECT * FROM carts JOIN carts_items ON uuid = cart_id WHERE user_id = $1",
+      [req.params.user_id]
+    );
+
+    res.status(201).json(cart);
+  } catch (error: any) {
+    console.log(error.message);
+    res.json({ status: "error", msg: "Get cart failed" });
+  }
 };
 
-export { createCart };
+const addItemToCart = async (req: Request, res: Response) => {
+  try {
+    const {
+      cart_id,
+      item_price,
+      quantity_ordered,
+      user_note,
+    }: {
+      cart_id: String;
+      item_price: Number;
+      quantity_ordered: Number;
+      user_note: String;
+    } = req.body;
+    const addedItem = await pool.query(
+      "INSERT INTO carts_items (item_id, cart_id, item_price, quantity_ordered, user_note) VALUES ($1 ,$2, $3, $4, $5) RETURNING *",
+      [req.params.item_id, cart_id, item_price, quantity_ordered, user_note]
+    );
+
+    res.status(201).json({ msg: "Item added to cart", createdItem: addedItem.rows });
+  } catch (error: any) {
+    console.log(error.message);
+    res.json({ status: "error", msg: "Add item to cart failed" });
+  }
+};
+
+const updateCart = async (req: Request, res: Response) => {
+  try {
+    // insert item to cart
+    const add = await pool.query("INSERT INTO carts");
+  } catch (error: any) {
+    console.log(error.message);
+    res.json({ status: "error", msg: "Update cart failed" });
+  }
+};
+
+export { createCart, getCartById, addItemToCart };
--- Backend/src/routers/carts.ts
+++ Backend/src/routers/carts.ts
@@ -1,8 +1,11 @@
 import express from "express";
 const router = express.Router();
 
-import { createCart } from "../controllers/carts";
+import { createCart, getCartById, addItemToCart } from "../controllers/carts";
 
 router.put("/carts/:user_id", createCart);
+router.post("/carts/:user_id", getCartById);
+
+router.put("/carts/items/:item_id", addItemToCart);
 
 export default router;
