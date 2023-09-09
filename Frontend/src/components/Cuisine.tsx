47a33239da3cf318af6dd82205147ff14e4c8b02
Desmond Tong <tysdesmond2@gmail.com>
2023-09-09 12:15:56 +0800

    Add cuisines/category cards

--- Frontend/src/components/Cuisine.tsx
+++ Frontend/src/components/Cuisine.tsx
@@ -1,53 +1,34 @@
 import React from "react";
 
-const Cuisine: React.FC = () => {
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Props } from "../interfaces";
+
+const Cuisine: React.FC<Props> = (props) => {
   return (
     <>
-      <Card
-        sx={{ display: "flex", borderRadius: "1rem" }}
-        elevation={4}
-        className="item-card"
-      >
-        <Box sx={{ display: "flex", flexDirection: "column", flexGrow: "1" }}>
-          <CardContent sx={{ flex: "1 0 auto" }}>
-            <Typography variant="body1" fontWeight="medium">
-              {props.name}
-            </Typography>
-            <Typography
-              variant="body2"
-              color="text.secondary"
-              fontWeight="light"
-              height="2.7rem"
-            >
-              {props.description}
-            </Typography>
-            <Typography variant="body2" fontWeight="light">
-              S$ {props.item_price}
-            </Typography>
-          </CardContent>
-        </Box>
-
+      <Card sx={{ display: "flex", borderRadius: "0.5rem" }} elevation={0}>
         <CardMedia
           component="img"
-          sx={{ width: "8rem", height: "8rem", p: "1rem" }}
-          image="./sample-image.webp"
+          sx={{
+            width: "100%",
+            height: "100%",
+            aspectRatio: 1,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+          image={`./${props.category}.jpg`}
         />
-
-        <Tooltip title="Edit Item">
-          <IconButton
-            className="edit-btn"
-            size="small"
-            sx={{ m: "0.4rem" }}
-            style={{ backgroundColor: "var(--orange)" }}
-            onClick={() => setOpenUpdate(true)}
-          >
-            <BorderColorOutlinedIcon
-              fontSize="small"
-              sx={{ p: "0.1rem", color: "white" }}
-            />
-          </IconButton>
-        </Tooltip>
       </Card>
+      <Typography
+        variant="subtitle2"
+        textAlign="center"
+        mt="0.5rem"
+        gutterBottom
+      >
+        {props.category}
+      </Typography>
     </>
   );
 };
--- Frontend/src/pages_customer/Homepage.tsx
+++ Frontend/src/pages_customer/Homepage.tsx
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import NavBar from "../components/NavBar";
 
 import { Grid, Box, Typography } from "@mui/material";
 import TopBar from "../components/TopBar";
+import Cuisine from "../components/Cuisine";
+
+import useFetch from "../hooks/useFetch";
+import UserContext from "../context/user";
+import { data } from "../interfaces";
 
 const Homepage: React.FC = () => {
+  const fetchData = useFetch();
+  const userCtx = useContext(UserContext);
+
+  const [categories, setCategories] = useState<String[]>([]);
+
+  // endpoint
+  const getCategories = async () => {
+    const res: data = await fetchData(
+      "/api/categories",
+      undefined,
+      undefined,
+      userCtx?.accessToken
+    );
+
+    if (res.ok) {
+      setCategories(res.data);
+    } else {
+      //attempt to refresh to get new access token
+      // userCtx?.refresh();
+
+      // if failed to refresh
+      alert(JSON.stringify(res.data));
+    }
+  };
+
+  useEffect(() => {
+    getCategories();
+  }, []);
   return (
     <>
       <Box sx={{ display: "flex" }}>
@@ -14,7 +47,7 @@ const Homepage: React.FC = () => {
           sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}
         >
           <TopBar></TopBar>
-          <Grid container mt="1.5rem" alignItems="center">
+          <Grid container mt="1.5rem" alignItems="center" spacing={4}>
             <Grid item xs={8} sx={{ borderStyle: "solid" }}>
               BANNER 1
             </Grid>
@@ -23,11 +56,20 @@ const Homepage: React.FC = () => {
             </Grid>
 
             <Grid item xs={12}>
-              <Typography variant="h4">Cuisines</Typography>
+              <Typography variant="h4" gutterBottom>
+                Cuisines
+              </Typography>
             </Grid>
-            
+            {categories.map((category, idx) => (
+              <Grid item xs={1.5} key={idx}>
+                <Cuisine category={category}></Cuisine>
+              </Grid>
+            ))}
+
             <Grid item xs={12}>
-              <Typography variant="h4">Restaurants</Typography>
+              <Typography variant="h4" gutterBottom>
+                Restaurants
+              </Typography>
             </Grid>
           </Grid>
         </Box>
